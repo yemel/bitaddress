1a3e8d10d9c7776597d9905b573c6a2d0ea5db09
Yemel Jardi <angel.jardi@gmail.com>
2014-12-20 16:11:12 -0300

    Add mongo and some basic views

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var mongo = require('mongo');
+var monk = require('monk');
+var db = monk('localhost:27017/bitaddress');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +27,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -14,11 +14,20 @@ function getRandomPath() {
 }
 
 var PRIV = new bitcore.HDPrivateKey();
-function getAddress(priv) {
-  var path = getRandomPath();
-
-  var derived = priv.derive(path).privateKey;
+function getAddress(xpriv, path) {
+  var derived = xpriv.derive(path).privateKey;
   return derived.toAddress().toString();
+}
+
+function createUser(username) {
+  var xpriv = new bitcore.HDPrivateKey();
+  var user = {
+    name: username,
+    email: username + '@gmail.com',
+    xpriv: xpriv.toString(),
+    payto: xpriv.privateKey.toAddress().toString()
+  };
+  return user;
 }
 
 
@@ -27,9 +36,53 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/yemel', function(req, res) {
-  var address = getAddress(PRIV);
-  res.render('index', { title: address });
+router.get('/users', function(req, res) {
+  var Users = req.db.get('users');
+  Users.find({}, {}, function(err, users) {
+    res.render('users', {users: users});
+  });
+});
+
+router.get('/addresses', function(req, res) {
+  var Addresses = req.db.get('addresses');
+  Addresses.find({}, {}, function(err, addresses) {
+    res.render('addresses', {addresses: addresses});
+  });
+});
+
+router.get('/user/:username', function(req, res) {
+  var Users = req.db.get('users');
+  var Addresses = req.db.get('addresses');
+
+  Users.findOne({name: req.params.username}, onUser);
+
+  function onUser(err, user) {
+    if (err) throw err;
+
+    var xpriv = new bitcore.HDPrivateKey(user.xpriv);
+    var path = getRandomPath();
+    var address = getAddress(xpriv, path);
+
+    Addresses.insert({
+      address: address,
+      path: path,
+      user: user._id
+    }, onInsert);
+  }
+
+  function onInsert(err, address) {
+    if (err) throw err;
+    res.render('index', { title: address.address });
+  }
+});
+
+router.get('/create/:username', function(req, res) {
+  var Users = req.db.get('users');
+  var user = createUser(req.params.username);
+  Users.insert(user, function(err, user) {
+    console.log(err);
+    res.redirect('/users');
+  });
 });
 
 module.exports = router;
